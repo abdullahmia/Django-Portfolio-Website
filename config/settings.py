2f2ef4603338c9dccb4133d7b7c8f78c2993c57c
darknoob1971 <abdullah149161@bsdi-bd.org>
2021-03-13 12:48:36 +0600

    fix image not showing

--- config/settings.py
+++ config/settings.py
@@ -132,8 +132,11 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATICFILES_STORAGE = 'whitenoise.storge.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = [
+	os.path.join(BASE_DIR, 'static')
+]
+# STATICFILES_STORAGE = 'whitenoise.storge.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
